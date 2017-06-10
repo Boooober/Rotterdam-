cabc7165062dc3e506dbe0635cf50a345bde40b4
Mykyta Slobodian <Mykyta_Slobodian@epam.com>
2017-06-10 16:27:03 +0300

    Add ACL contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,17 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
+const AddressListLib = artifacts.require('./lib/AddressList.sol');
+const Owned = artifacts.require('./common/Owned.sol');
+const Destroyable = artifacts.require('./common/Destroyable.sol');
+const Object = artifacts.require('./common/Object.sol');
+const ACLStorage = artifacts.require('./acl/ACLStorage.sol');
+const ACL = artifacts.require('./acl/ACL.sol');
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+  deployer.deploy(AddressListLib);
+
+  deployer.deploy(Owned);
+  deployer.deploy(Destroyable);
+  deployer.deploy(Object);
+  deployer.deploy(ACLStorage);
+  deployer.deploy(ACL);
+  deployer.autolink();
 };
